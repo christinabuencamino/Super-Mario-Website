3333a4275f10fc1af6120ff8bc0fe0eb61f6934c
christinabuencamino <c.buencamino123@gmail.com>
2023-02-16 18:17:57 -0500

    Saving progress - started home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,35 @@
 import './css/Home.css'
 
-import Venice from "./images/venice-home.jpeg"
+import Venice from "./images/venice_gondola.jpeg"
+import MoreVenice from "./images/more-venice.jpeg"
 
 function Home() {
     return (
-        <div id="image-container">
-            <img src={Venice} alt="spongebob house" />
-            <div id="Home-Intro">
-                <p>
-                    uieuhihiuhfi<br/>ewufhowefhoweu<br/>efewkfjnwkejfnkwejnf
+        <div>
+            <div id="image-container">
+                <img src={Venice} alt="Venice Canal Picture" id="Home-image"/>
+                <div id="Home-Intro">
+                    <p id="title">
+                        Plan Your Trip To Venice!
                     </p>
+                    <p>
+                        Use this website to find hotels, attractions, and suitable gifts for loved ones.
+                    </p>
+                </div>
             </div>
+            <br></br><br></br><br></br>
+            <div id="image-container">
+                <img src={MoreVenice} alt="Venice Canal Picture" id="Home-image"/>
+                <div id="Home-Intro">
+                    <p id="title">
+                        Why Venice?
+                    </p>
+                    <p>
+                        Venice blah blah blah
+                    </p>
+                </div>
+            </div>
+            <br></br><br></br><br></br>
         </div>
     );
   }
