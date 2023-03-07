cf24e33cd871206a00c0072aacf8f16105643a4b
christinabuencamino <c.buencamino123@gmail.com>
2023-03-07 17:30:27 -0500

    Changed theme, home page kind of donegit status

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import './css/Home.css'
-import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function Home() {
     return (
       <>        
-        <main><div id="home"><h1>Welcome!</h1></div></main>
-        <footer><img id="pipe" src={require("./images/pipe.gif")} /></footer>
+        {/* <main><div id="home"><h1>Welcome to Mushroom Kingdom!<br></br>Use this website to plan your perfect visit!</h1></div></main> */}
+        {/* <footer><img id="pipe" src={require("./images/pipe.gif")} /></footer> */}
+        <main>
+        <div style={{display: "flex", flexDirection: "row", height: '7vw', justifyContent: 'space-between'}}>
+        <img id="left-lakitu" src={require("./images/lakitu.png")} alt="lakitu"></img>
+        <img id="right-lakitu" src={require("./images/lakitu.png")} alt="lakitu"></img>
+        </div>
+        <div>
+          <h1 id="message-box">Welcome to Mushroom Kingdom!<br></br>Use the tabs above to plan out your<br/> perfect trip today!</h1>
+        </div>
+        </main>
       </>
     );
   }
